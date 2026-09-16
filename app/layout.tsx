e48976c2869e6e00dad73194f210a8d0c3029196
petar-shotz <134802172+petar-shotz@users.noreply.github.com>
2026-09-16 11:51:09 +0200

    Add metadata for icons in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
+export const metadata: Metadata = {
+  icons: {
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
+  },
+};
 
 export const viewport: Viewport = {
   themeColor: "#effa82",
